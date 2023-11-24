d9a376e072e4e6d17239cedb887e46e60a99886a
Nimish <msnimish24@gmail.com>
2023-11-24 08:15:58 +0530

    Started OOPS section

---
+++ OOPs/src/Car.java
@@ -0,0 +1,16 @@
+public class Car {
+    String model = "Hatchback";
+    String color = "Black";
+    int price;
+
+    void drive(){
+        System.out.println("Zoom zoom zooom!!!");
+    }
+    void lock(){
+        System.out.println("Car is now locked");
+    }
+
+    void unlock(){
+        System.out.println("Car is now unlocked");
+    }
+}
--- OOPs/src/Main.java
+++ OOPs/src/Main.java
@@ -1,5 +1,26 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Car c1 = new Car();
+        Car c2 = new Car();
+
+//        Reading properties
+
+        System.out.println("C1 Properties ------------------------------------------");
+        System.out.println(c1.color);
+        System.out.println(c1.model);
+        System.out.println(c1.price);
+
+        System.out.println("C2 Properties ------------------------------------------");
+        c2.color = "Red";  // Updating c2 object property which won't affect c1
+        System.out.println(c2.color);
+        System.out.println(c2.model);
+        System.out.println(c2.price);
+
+//        Executing class methods
+        c1.drive();
+        c1.lock();
+        c1.unlock();
+
+
     }
 }
