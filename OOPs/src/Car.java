a125f34608311ca9edfa8006f7b02af55826b635
Nimish <msnimish24@gmail.com>
2023-11-26 03:30:11 +0530

    OOPS Continued...

--- OOPs/src/Car.java
+++ OOPs/src/Car.java
@@ -1,16 +1,43 @@
 public class Car {
-    String model = "Hatchback";
-    String color = "Black";
-    int price;
+    String model;
+    String color;
+    private int price;
+    boolean isLocked = false;
 
+    Car(){
+        // This is the default constructor function
+        System.out.println("Default constructor called");
+        model = "Hatchback";
+        color = "Black";
+        setPrice(10000);
+    }
+    Car(String modelName, String colorName, int priceValue){
+        // This is the default constructor function
+        System.out.println("Parameterised constructor called");
+        model = modelName;
+        color = colorName;
+        setPrice(priceValue);
+    }
+//  Getter for price property
+    int getPrice(){
+        return price;
+    }
+
+//  Setter for price property
+    void setPrice(int priceValue){
+        if(priceValue<0) priceValue = 0;
+        price = priceValue;
+    }
     void drive(){
         System.out.println("Zoom zoom zooom!!!");
     }
     void lock(){
+        isLocked = true;
         System.out.println("Car is now locked");
     }
 
     void unlock(){
+        isLocked = false;
         System.out.println("Car is now unlocked");
     }
 }
--- OOPs/src/Main.java
+++ OOPs/src/Main.java
@@ -1,26 +1,40 @@
 public class Main {
     public static void main(String[] args) {
-        Car c1 = new Car();
+        Car c1 = new Car("Sedan", "Blue", 14000);
         Car c2 = new Car();
 
-//        Reading properties
-
+//        24-11-2023
+        /*Reading properties
         System.out.println("C1 Properties ------------------------------------------");
         System.out.println(c1.color);
         System.out.println(c1.model);
-        System.out.println(c1.price);
+        System.out.println(c1.getPrice());
 
         System.out.println("C2 Properties ------------------------------------------");
         c2.color = "Red";  // Updating c2 object property which won't affect c1
         System.out.println(c2.color);
         System.out.println(c2.model);
-        System.out.println(c2.price);
+        System.out.println(c2.getPrice());*/
 
-//        Executing class methods
+        /*Executing class methods
         c1.drive();
         c1.lock();
-        c1.unlock();
+        c2.unlock();
+        System.out.println(c1.isLocked);
+        System.out.println(c2.isLocked);
 
+        c1.setPrice(7_500);
+        c2.setPrice(-3_000);
+        System.out.println(c1.getPrice());
+        System.out.println(c2.getPrice());*/
+
+//        26-11-2023
+        System.out.println(c1.getPrice());
+        System.out.println(c1.model);
+        System.out.println(c1.color);
+        System.out.println(c2.getPrice());
+        System.out.println(c2.model);
+        System.out.println(c2.color);
 
     }
 }
